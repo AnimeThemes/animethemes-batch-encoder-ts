4872672635777cbb94c817480b8cb290ea605af3
Kyrch <kyrchk5@gmail.com>
2025-07-17 13:56:50 -0300

    feat: added more audio filters

--- src/audio-filter.ts
+++ src/audio-filter.ts
@@ -1,4 +1,4 @@
-import { number } from "@inquirer/prompts";
+import { input, number } from "@inquirer/prompts";
 
 interface AudioFilterConfig<T> {
     label: string;
@@ -26,23 +26,32 @@ const audioFilters = [
     createAudioFilter({
         label: "Fade In",
         prompt: async () => ({
-            duration: await number({
-                message: "Duration",
-                required: true,
-            }),
+            exponential: await number({ message: "Exponential Time", required: true }),
         }),
-        toString: (options) => `afade=d=${options.duration}:curve=exp`,
+        toString: (options) => `afade=d=${options.exponential}:curve=exp`,
     }),
     createAudioFilter({
         label: "Fade Out",
         prompt: async () => ({
             startTime: await number({ message: "Start Time", required: true }),
-            duration: await number({
-                message: "Duration",
-                required: true,
-            }),
+            exponential: await number({ message: "Exponential Time", required: true }),
         }),
-        toString: (options) => `afade=t=out:st=${options.startTime}:d=${options.duration}`,
+        toString: (options) => `afade=t=out:st=${options.startTime}:d=${options.exponential}`,
+    }),
+    createAudioFilter({
+        label: "Mute",
+        prompt: async () => ({
+            startTime: await number({ message: "Start Time", default: 0 }),
+            endTime: await number({ message: "End Time", required: true }),
+        }),
+        toString: (options) => `volume=enable='between(t,${options.startTime},${options.endTime})':volume=0`,
+    }),
+    createAudioFilter({
+        label: "Custom",
+        prompt: async () => ({
+            text: await input({ message: "Filter", required: true }),
+        }),
+        toString: (options) => options.text,
     }),
 ] satisfies Array<AudioFilter>;
 
