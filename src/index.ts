59e3e2fb7dcee6ce2c50f315c7a1c051f5e55c73
Kyrch <kyrchk5@gmail.com>
2026-06-08 11:26:33 -0300

    refactor

--- src/index.ts
+++ src/index.ts
@@ -37,9 +37,7 @@ program
 program
     .command("update")
     .description("Update the CLI to the latest version")
-    .action(async () => {
-        await update();
-    });
+    .action(async () => await update());
 
 await program.parseAsync();
 
